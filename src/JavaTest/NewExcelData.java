be11e85c4aee40283cb76ceffc3bf7fde4954d83
TMVMalai <tperumal2205@gmail.com>
2023-12-22 13:33:59 +0530

    Commit message

--- src/JavaTest/NewExcelData.java
+++ src/JavaTest/NewExcelData.java
@@ -1,11 +1,14 @@
 package JavaTest;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
 
@@ -46,6 +49,26 @@ public void excelWritter() throws IOException {
 			e.printStackTrace();
 		}
 		
+	}
+	@Test
+	public void main() {
+		String file = "data.xlsx";
+		try {
+			FileInputStream inputStream = new FileInputStream(file);
+			Workbook workbook = new XSSFWorkbook(inputStream);
+			Sheet sheet = workbook.getSheetAt(0);
+			Row row = sheet.getRow(1);
+			Cell cell = row.getCell(0);
+			if(cell.getCellType()==CellType.STRING) {
+				System.out.println(cell.getStringCellValue());
+			}
+			else {
+				System.out.println(cell.getNumericCellValue());
+			}
+			workbook.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
